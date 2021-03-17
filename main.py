6a53069c81b0750085bbee5f70e938a6ad336fdc
Adebayo <aaderibigbe@gmail.com>
2021-03-17 22:48:49 +0100

    runtime.txt to specify python version to use

--- main.py
+++ main.py
@@ -28,6 +28,6 @@ def predict():
     return jsonify(result)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port='5000')
+# if __name__ == '__main__':
+#     app.run(debug=True, host='127.0.0.1', port='5000')
 
