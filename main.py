3c1018a6153cd375d4fff028950442f39a139594
Adebayo <aaderibigbe@gmail.com>
2021-03-17 19:33:06 +0100

    make it better

---
+++ main.py
@@ -0,0 +1,33 @@
+import pickle
+from flask import Flask, request, jsonify
+from model_files.ml_model import predict_mpg
+
+##creating a flask app and naming it "app"
+app = Flask('app')
+
+@app.route('/test', methods=['GET'])
+def test():
+    return 'Pinging Model Application!!'
+
+@app.route('/', methods=['GET'])
+def my_home():
+    return 'Welcome to the Home Page!!'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    vehicle = request.get_json()
+    print(vehicle)
+    with open('model_files\model.pkl', 'rb') as f_in:
+        model = pickle.load(f_in)
+        f_in.close()
+    predictions = predict_mpg(vehicle, model)
+
+    result = {
+        'mpg_prediction': list(predictions) 
+    }
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port='5000')
+
---
+++ test.py
@@ -0,0 +1,16 @@
+import requests
+# data set for  3 vehicles 
+
+vehicle_config = {
+    'Cylinders': [4, 6, 8],
+    'Displacement': [155.0, 160.0, 165.5],
+    'Horsepower': [93.0, 130.0, 98.0],
+    'Weight': [2500.0, 3150.0, 2600.0],
+    'Acceleration': [15.0, 14.0, 16.0],
+    'Model Year': [81, 80, 78],
+    'Origin': [3, 2, 1]
+}
+
+url = 'http://127.0.0.1:5000/predict'
+r = requests.post(url, json = vehicle_config)
+print(r.text.strip())
